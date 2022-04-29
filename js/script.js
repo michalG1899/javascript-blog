0c272eb7879d51049ca85c50e3e3a69b09e9b4b7
Michal Gebski <gebski.michal@gmail.com>
2022-04-29 12:14:33 +0200

    removing active class from element

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
-'use strict';
+"use strict";
 
-function titleClickHandler(event){
-  console.log('Link was clicked!');
+function titleClickHandler(event) {
+  console.log("Link was clicked!");
   console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll(".titles a.active");
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
 
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+//   const activeArticles = document.querySelectorAll(".titles a.active");
+  const activeArticles = document.querySelectorAll(".post");
+
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove("active");
+  }
 
   /* get 'href' attribute from the clicked link */
 
@@ -17,8 +29,8 @@ function titleClickHandler(event){
   /* add class 'active' to the correct article */
 }
 
-const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll(".titles a");
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+for (let link of links) {
+  link.addEventListener("click", titleClickHandler);
 }
